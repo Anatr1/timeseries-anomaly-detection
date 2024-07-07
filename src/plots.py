f78fc7f93657a27a5533f3111421c495724e17b5
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-07-07 20:57:29 +0200

    added more evaluation method, and rerun all

--- src/plots.py
+++ src/plots.py
@@ -302,3 +302,24 @@ def plot_anomalies_over_time(X_test, anomaly_scores, anomalies_detected, freq):
     # Print details of top anomalies
     print("Top", N, "Anomalies:")
     print(top_anomalies[['timestamp', 'anomaly_score'] + list(features_to_plot)])
+    
+def plot_roc_curve(y_true, y_scores):
+    # Generate many threshold values to create a smoother curve
+    thresholds = np.linspace(0, 1, 500)
+    fpr, tpr, _ = roc_curve(y_true, y_scores)
+    
+    # Calculate the area under the curve
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
